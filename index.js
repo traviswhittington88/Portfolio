0edb91f441b421e9b1b77127d85cd7be62ec002a
traviswhittington88 <traviswhittington@ymail.com>
2019-08-11 12:50:29 -0500

    added scroll

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-$('.menu-toggle').click(function(event){
+$('.menu-toggle').click(function(event) {
     $('.site-nav').toggleClass('site-nav--open',500);
     $(this).toggleClass('open');
 });
+
+$("a").click(function(){
+  let pageId = $(this).attr("data-page");
+  $("html, body").animate({ scrollTop: $("#"+pageId).offset().top }, 1000);
+});
+      
+    
