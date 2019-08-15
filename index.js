cb70abef3e6ea0de354a91753cfacbfa551d2c9f
traviswhittington88 <traviswhittington@ymail.com>
2019-08-15 18:32:25 -0500

    extended revision

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-$('.menu-toggle').click(function(event) {
-    $('.site-nav').toggleClass('site-nav--open',500);
-    $(this).toggleClass('open');
+$('.menu-toggle, .nav-link').click(function(event) {
+  console.log('click!');
+  $('.site-nav').toggleClass('site-nav--open',500);
+  $(this).toggleClass('open');
 });
 
-$(".link").click(function(){
-  let pageId = $(this).attr("data-page");
-  $("html, body").animate({ scrollTop: $("#"+pageId).offset().top }, 1000);
+$(".nav-link").click(function(){
+  let prefix = "#";
+  let pageId = prefix + $(this).attr("data-page");
+  console.log(pageId);
+  $("html, body").animate({ scrollTop: $(pageId).offset().top - 60 }, 1000);
 });
       
     
