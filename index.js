d200a34c18991def224a69d2981cb989b8447b31
traviswhittington88 <traviswhittington@ymail.com>
2020-03-29 21:24:07 -0500

    removed console logs

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 $('.menu-toggle, .nav-link').click(function(event) {
-  console.log('click!');
-  $('.site-nav').toggleClass('site-nav--open',500);
+  $('.site-nav').toggleClass('site-nav--open', 500);
   $('.menu-toggle').toggleClass('open');
 });
 
-$(".nav-link").click(function(){
-  let prefix = "#";
-  let pageId = prefix + $(this).attr("data-page");
-  console.log(pageId);
-  $("html, body").animate({ scrollTop: $(pageId).offset().top - 60 }, 1000);
+$('.nav-link').click(function() {
+  let prefix = '#';
+  let pageId = prefix + $(this).attr('data-page');
+  $('html, body').animate({ scrollTop: $(pageId).offset().top - 60 }, 1000);
 });
-      
-    
